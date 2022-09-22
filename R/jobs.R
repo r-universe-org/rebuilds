f42a6d9fe7c156acd393224d0a8dda0db436d1b2
Jeroen Ooms <jeroenooms@gmail.com>
2022-09-22 13:19:09 +0200

    add wrapper to rebuild all docs

--- R/jobs.R
+++ R/jobs.R
@@ -5,6 +5,7 @@ rerun_one_job <- function(url, name = 'Deploy to package server'){
   endpoint <- sub("https://github.com/", "/repos/", url)
   job <- get_job_info(endpoint, name = name)
   endpoint <- sub("/actions/.*", paste0("/actions/jobs/", job$id, '/rerun'), endpoint)
+  message("Triggering: ", endpoint)
   gh::gh(endpoint, .method = 'POST')
 }
 
@@ -14,4 +15,16 @@ get_job_info <- function(url, name){
   jobs <- Filter(function(x) grepl(name, x$name, ignore.case = TRUE), res$jobs)
   if(!length(jobs)) stop("No job found matching name: ", name)
   return(jobs[[1]])
+}
+
+rerun_one_job_for_all <- function(universe, job_name){
+  descriptions <- jsonlite::stream_in(url(sprintf("https://%s.r-universe.dev/stats/descriptions", universe)))
+  urls <- descriptions[['_builder']]$url
+  lapply(urls, function(url){
+    try(rerun_one_job(url, job_name))
+  })
+}
+
+rebuild_all_ropensci_docs <- function(){
+  rerun_one_job_for_all('ropensci', 'documentation')
 }
